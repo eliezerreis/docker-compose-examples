1623ed3acdfb12a482b02b4c5fb3caa5782d8688
Eliezer Oliveira <eliezerreis@gmail.com>
2020-04-06 17:44:44 -0400

    Include script to load password from secret file if user included _FILE on password attributes from stack

--- drupal_postgresql/drupal-init.sh
+++ drupal_postgresql/drupal-init.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'DRUPAL_PASSWORD'
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE ROLE $DRUPAL_USER WITH LOGIN NOINHERIT ENCRYPTED PASSWORD '$DRUPAL_PASSWORD';
     CREATE DATABASE $DRUPAL_DB;
